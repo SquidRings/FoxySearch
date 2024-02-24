44a32e3b6c91a5027910fc9ce776d7ebb9aa7d4a
Haris-Ul-Haq Chaudhry <chaudhryharis2@outlook.fr>
2024-02-24 18:08:18 +0100

    major update

--- server.py
+++ server.py
@@ -10,21 +10,21 @@ indexer.load_data()
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/search')
-def search():
-    query = request.args.get('q', '')
-    results = indexer.search(query)
-    return jsonify(results)
-
-
 # @app.route('/search')
 # def search():
 #     query = request.args.get('q', '')
-#     start_time = time.time()
 #     results = indexer.search(query)
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     return jsonify({'results': results, 'elapsed_time': elapsed_time})
+#     return jsonify(results)
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', '')
+    start_time = time.time()
+    results = indexer.search(query)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return jsonify({'results': results, 'elapsed_time': elapsed_time})
 
 if __name__ == "__main__":
     app.run(debug=False, port=8080, host="0.0.0.0")
