77cea030fd079c6513d14fdef3bc68e82ee435f1
Haris-Ul-Haq Chaudhry <chaudhryharis2@outlook.fr>
2024-02-24 09:44:38 +0100

    add proto

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from indexer import Indexer
+import time
 
 app = Flask(__name__)
 indexer = Indexer('output.json')
@@ -14,6 +15,16 @@ def search():
     query = request.args.get('q', '')
     results = indexer.search(query)
     return jsonify(results)
+
+
+# @app.route('/search')
+# def search():
+#     query = request.args.get('q', '')
+#     start_time = time.time()
+#     results = indexer.search(query)
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     return jsonify({'results': results, 'elapsed_time': elapsed_time})
 
 if __name__ == "__main__":
     app.run(debug=False, port=8080, host="0.0.0.0")
